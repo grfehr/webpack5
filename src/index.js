744322e8ebe2487f3b8226457c7c21851c47cd4a
Gary Fehr <gfehr@key2act.com>
2024-05-16 14:02:40 -0500

    Section 23

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
-// import addImage from './add-image.js';
+import Heading from './components/heading/heading.js';
+
+const heading = new Heading();
+heading.render();
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
-
-// addImage();
