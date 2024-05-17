065939ba7f66e47996e3ddd9d1098645bfc1189d
Gary Fehr <gfehr@key2act.com>
2024-05-17 12:55:51 -0500

    Section 40

--- src/time.js
+++ src/time.js
@@ -1,5 +1,6 @@
 import Heading from "./components/heading/heading";
 import TimeImage from "./components/time-image/time-image";
+import React from 'react';
 
 const heading = new Heading();
 heading.render('time image');
